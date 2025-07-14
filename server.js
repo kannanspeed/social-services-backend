2c4b20a4cb20744703a28b9106c40ea8ef91bc17
kannanspeed <kannangod123@gmail.com>
2025-07-14 20:30:24 +0530

    Fix: Add body-parser middleware for request parsing

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
 
@@ -45,6 +46,8 @@ const corsOptions = {
 
 // Middleware
 app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Data storage files
